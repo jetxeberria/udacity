0be478737ad3c1552b16125d974c1bff09671c21
jetxeberria-dasnano <jetxeberria@das-nano.com>
2021-05-27 08:08:10 +0200

    Add funtions for l2 final exercise #0

--- tests/test_lesson_2_final_exercise.py
+++ tests/test_lesson_2_final_exercise.py
@@ -1,7 +1,7 @@
 import pytest
 
 from udacity import lesson_2_final_exercise as l2
-from tests.helpers.lesson_2 import english_words_small, english_anagrams_small
+from tests.helpers.lesson_2_final_exercise import keymap
 
 
 @pytest.mark.parametrize(
@@ -13,18 +13,44 @@ def test_parse_content(string, parsed_dict):
     assert parsed == parsed_dict
 
 @pytest.mark.parametrize(
-    ["input_dict", "tree"],
-    [({"ban":10, "band":5, "bar":14, "can":32, "candy":7}, {"b": {"a": {"n": {"$ban": 10, "d": {"$band": 5}}, "r": {"$bar": 14}}}, "c": {"a": {"n": {"$can": 32,"d": {"y": {"$candy": 7}}}}}})]
+    ["input_dict", "trie"],
+    [({"ban":10, "band":5, "bar":14, "can":32, "candy":7},
+    {"b": {"a": {"n": {"$ban": 10, "d": {"$band": 5}}, "r": {"$bar": 14}}}, "c": {"a": {"n": {"$can": 32,"d": {"y": {"$candy": 7}}}}}})]
 )
-def test_make_tree(input_dict, tree):
-    tree = {}
-    subtree = tree
-    for k, v in input_dict.items():
-        for level, c in enumerate(list(k)):
-            if c not in subtree:
-                subtree[c] = {}
-                subtree = subtree[c]
-            if level == len(k)-1:
-                subtree[c] = v
-    print(input_dict)
-    print(tree)
+def test_make_tree(input_dict, trie):
+    output_tree = l2.make_tree(input_dict)
+    assert output_tree == trie
+
+
+@pytest.mark.parametrize(
+    ["trie", "numbers", "words"],
+    [({"b": {"a": {"n": {"$ban": 10, "d": {"$band": 5}},
+                   "r": {"$bar": 14}}},
+       "c": {"a": {"n": {"$can": 32,"d": {"y": {"$candy": 7}}}}}},
+       2263,
+      [('candy', 7),
+       ('band', 5)])]
+)
+def test_predict(trie, numbers, words):
+    # candidates = []
+    # subtrie = trie
+    candidate_words = l2.predict(trie, numbers)
+    assert candidate_words == words
+
+
+@pytest.mark.parametrize(
+    ["subtries", "words"],
+    [([{"n": {"$ban": 10, "d": {"$band": 5}},
+                   "r": {"$bar": 14}},
+       {"y": {"$candy": 7}}],
+      {('ban', 10),
+       ('bar', 14),
+       ('candy', 7),
+       ('band', 5)})]
+)
+def test_extract_words(subtries, words):
+    found_words = l2.extract_words(subtries)
+    assert found_words
+    assert all(word in words for word in found_words)
+    print(found_words)
+    assert 0
--- udacity/lesson_2_final_exercise.py
+++ udacity/lesson_2_final_exercise.py
@@ -1,5 +1,17 @@
 from collections import defaultdict
 
+
+keymap = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+    }
+
 def parse_content(content):
     parsed = {}
     lines = content.split("\n")
@@ -8,7 +20,57 @@ def parse_content(content):
 
 
 def make_tree(words):
-    pass
+    tree = {}
+    for k, v in words.items():
+        subtree = tree
+        for level, c in enumerate(list(k)):
+            if c not in subtree:
+                subtree[c] = {}
+            subtree = subtree[c]
+            if level == len(k)-1:
+                subtree['$'+k] = v
+    return tree
 
-def predict(tree, numbers):
-    pass
+def predict(trie, numbers):
+    candidate_subtries = [trie]
+    for number in str(numbers):
+        match_found = False
+        for k in keymap[number]:
+            for i, subtrie in enumerate(candidate_subtries):
+                if k == subtrie:
+                    candidate_subtries.append(subtrie[k])
+                    candidate_subtries.pop(i)
+                    match_found = True
+        if not match_found:
+            candidate_subtries.clear()
+    candidate_words = extract_words(candidate_subtries)
+    sorted_words = sort_words(candidate_words)
+    return candidate_words
+
+
+def extract_words(subtries):
+    words = []
+    for subtrie in subtries:
+        sub_words = list(words_generator(subtrie))
+        if sub_words:
+            words.append(sub_words)
+    return words
+
+def words_generator(subtrie):
+    for k, v in subtrie.items():
+        if k.startswith("$"):
+            yield (k, v)
+        else:
+            subtrie = words_generator(v)
+    return
+
+
+def sort_words(words):
+    highest = words[0]
+    sorted_words = []
+    for word in words:
+        for word2 in words:
+            if word2[1] >= highest[1] and word2[0] != highest[0]:
+                highest = word2
+        sorted_words.append(highest)
+    return sorted_words
