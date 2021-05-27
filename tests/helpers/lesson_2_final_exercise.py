45440f9d32a6fbf9e953a2fec9bbb1b7b557c752
jetxeberria-dasnano <jetxeberria@das-nano.com>
2021-05-27 08:08:38 +0200

    Include helpers for tests #0

---
+++ tests/helpers/lesson_2_final_exercise.py
@@ -0,0 +1,14 @@
+import pytest
+
+@pytest.fixture
+def keymap():
+    return {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+    }
