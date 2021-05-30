14ea607e4d667f5e156903f68b8bac803f3c629f
jetxeberria-dasnano <jetxeberria@das-nano.com>
2021-05-30 18:49:11 +0200

    Update test suite #0

--- tests/test_lesson_2_final_exercise.py
+++ tests/test_lesson_2_final_exercise.py
@@ -4,18 +4,37 @@ from udacity import lesson_2_final_exercise as l2
 from tests.helpers.lesson_2_final_exercise import keymap
 
 
+def assert_lists_are_equal(list1, list2):
+    assert len(list1) == len(list2)
+    for i, element in enumerate(list1):
+        assert element == list2[i]
+
+
 @pytest.mark.parametrize(
     ["string", "parsed_dict"],
-    [("ban 10\nband 5\nbar 14\ncan 32\ncandy 7", {"ban":10, "band":5, "bar":14, "can":32, "candy":7})]
+    [
+        (
+            "ban 10\nband 5\nbar 14\ncan 32\ncandy 7",
+            {"ban": 10, "band": 5, "bar": 14, "can": 32, "candy": 7},
+        )
+    ],
 )
 def test_parse_content(string, parsed_dict):
     parsed = l2.parse_content(string)
     assert parsed == parsed_dict
 
+
 @pytest.mark.parametrize(
     ["input_dict", "trie"],
-    [({"ban":10, "band":5, "bar":14, "can":32, "candy":7},
-    {"b": {"a": {"n": {"$ban": 10, "d": {"$band": 5}}, "r": {"$bar": 14}}}, "c": {"a": {"n": {"$can": 32,"d": {"y": {"$candy": 7}}}}}})]
+    [
+        (
+            {"ban": 10, "band": 5, "bar": 14, "can": 32, "candy": 7},
+            {
+                "b": {"a": {"n": {"$ban": 10, "d": {"$band": 5}}, "r": {"$bar": 14}}},
+                "c": {"a": {"n": {"$can": 32, "d": {"y": {"$candy": 7}}}}},
+            },
+        )
+    ],
 )
 def test_make_tree(input_dict, trie):
     output_tree = l2.make_tree(input_dict)
@@ -24,12 +43,16 @@ def test_make_tree(input_dict, trie):
 
 @pytest.mark.parametrize(
     ["trie", "numbers", "words"],
-    [({"b": {"a": {"n": {"$ban": 10, "d": {"$band": 5}},
-                   "r": {"$bar": 14}}},
-       "c": {"a": {"n": {"$can": 32,"d": {"y": {"$candy": 7}}}}}},
-       2263,
-      [('candy', 7),
-       ('band', 5)])]
+    [
+        (
+            {
+                "b": {"a": {"n": {"$ban": 10, "d": {"$band": 5}}, "r": {"$bar": 14}}},
+                "c": {"a": {"n": {"$can": 32, "d": {"y": {"$candy": 7}}}}},
+            },
+            2263,
+            [("candy", 7), ("band", 5)],
+        )
+    ],
 )
 def test_predict(trie, numbers, words):
     # candidates = []
@@ -40,17 +63,63 @@ def test_predict(trie, numbers, words):
 
 @pytest.mark.parametrize(
     ["subtries", "words"],
-    [([{"n": {"$ban": 10, "d": {"$band": 5}},
-                   "r": {"$bar": 14}},
-       {"y": {"$candy": 7}}],
-      {('ban', 10),
-       ('bar', 14),
-       ('candy', 7),
-       ('band', 5)})]
+    [
+        (
+            [
+                {"n": {"$ban": 10, "$bus": 11, "d": {"$band": 5}}, "r": {"$bar": 14}},
+                {"y": {"$candy": 7, "d": {"$band": 5}}},
+            ],
+            {
+                ("ban", 10),
+                ("bus", 11),
+                ("bar", 14),
+                ("candy", 7),
+                ("band", 5),
+                ("band", 5),
+            },
+        )
+    ],
 )
 def test_extract_words(subtries, words):
     found_words = l2.extract_words(subtries)
     assert found_words
     assert all(word in words for word in found_words)
-    print(found_words)
-    assert 0
+
+
+@pytest.mark.parametrize(
+    ["words", "sorted_words"],
+    [
+        (
+            {
+                ("ban", 10),
+                ("bus", 11),
+                ("bar", 14),
+                ("candy", 7),
+                ("band", 5),
+            },
+            [
+                ("bar", 14),
+                ("bus", 11),
+                ("ban", 10),
+                ("candy", 7),
+                ("band", 5),
+            ],
+        )
+    ],
+)
+def test_sort_words(words, sorted_words):
+    print(words)
+    sorted_output = l2.sort_words(words)
+    assert_lists_are_equal(sorted_words, sorted_output)
+
+
+def test_predict_numbers(trie, numbers):
+    @pytest.mark.parametrize(
+    ["trie", "numbers", "words"],
+    [
+        (
+            {
+                "b": {"a": {"n": {"$ban": 10, "d": {"$band": 5}}, "r": {"$bar": 14}}},
+                "c": {"a": {"n": {"$can": 32, "d": {"y": {"$candy": 7}}}}},
+            },
+            2263,
--- udacity/lesson_2_final_exercise.py
+++ udacity/lesson_2_final_exercise.py
@@ -2,20 +2,21 @@ from collections import defaultdict
 
 
 keymap = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz'
-    }
+    "2": "abc",
+    "3": "def",
+    "4": "ghi",
+    "5": "jkl",
+    "6": "mno",
+    "7": "pqrs",
+    "8": "tuv",
+    "9": "wxyz",
+}
+
 
 def parse_content(content):
     parsed = {}
     lines = content.split("\n")
-    parsed = {l.split(" ")[0]:int(l.split(" ")[1]) for l in lines}
+    parsed = {l.split(" ")[0]: int(l.split(" ")[1]) for l in lines}
     return parsed
 
 
@@ -27,11 +28,19 @@ def make_tree(words):
             if c not in subtree:
                 subtree[c] = {}
             subtree = subtree[c]
-            if level == len(k)-1:
-                subtree['$'+k] = v
+            if level == len(k) - 1:
+                subtree["$" + k] = v
     return tree
 
+
 def predict(trie, numbers):
+    candidate_subtries = predict_numbers(trie, numbers)
+    candidate_words = extract_words(candidate_subtries)
+    sorted_words = sort_words(candidate_words)
+    return sorted_words
+
+
+def predict_numbers(trie, numbers):
     candidate_subtries = [trie]
     for number in str(numbers):
         match_found = False
@@ -43,9 +52,7 @@ def predict(trie, numbers):
                     match_found = True
         if not match_found:
             candidate_subtries.clear()
-    candidate_words = extract_words(candidate_subtries)
-    sorted_words = sort_words(candidate_words)
-    return candidate_words
+    return candidate_subtries
 
 
 def extract_words(subtries):
@@ -53,24 +60,60 @@ def extract_words(subtries):
     for subtrie in subtries:
         sub_words = list(words_generator(subtrie))
         if sub_words:
-            words.append(sub_words)
-    return words
+            words.extend(sub_words)
+    return set(words)
 
+
+# def words_generator(subtrie):
+#     words = []
+#     for k, v in subtrie.items():
+#         if isinstance(v, dict):
+#             yield list(words_generator(v))[0]
+#         elif isinstance(v, list):
+#             for g in k:
+#                 yield list(words_generator(v))[0]
+#         if k.startswith("$"):
+#             yield (k, v)  # yield words
+#
+#
 def words_generator(subtrie):
+    words = []
     for k, v in subtrie.items():
+        if isinstance(v, dict):
+            for words in words_generator(v):
+                yield words
+        elif isinstance(v, list):
+            for g in k:
+                for words in words_generator(v):
+                    yield words
         if k.startswith("$"):
-            yield (k, v)
-        else:
-            subtrie = words_generator(v)
-    return
+            yield (k[1:], v)  # yield words
 
 
-def sort_words(words):
-    highest = words[0]
+#
+
+#
+# def words_generator(subtrie):
+# words = []
+# for k, v in subtrie.items():
+# if isinstance(v, dict):
+# words.append(list(words_generator(v))[0])
+# elif isinstance(v, list):
+# for g in k:
+# words.append(list(words_generator(v))[0])
+# if k.startswith("$"):
+# words.append((k, v))  # yield words
+# return words
+#
+#
+def sort_words(words_s):
+    words = list(words_s)
     sorted_words = []
-    for word in words:
+    for i in range(len(words)):
+        highest = words[0]
         for word2 in words:
-            if word2[1] >= highest[1] and word2[0] != highest[0]:
+            if word2[1] >= highest[1] and word2 is not highest:
                 highest = word2
         sorted_words.append(highest)
+        words.pop(words.index(highest))
     return sorted_words
